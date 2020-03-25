68358556f261719774f4af7aa224867d9d251667
chulminkw <55954384+chulminkw@users.noreply.github.com>
2020-03-25 22:39:30 +0900

    Add files via upload

---
+++ data/util/install_tf115.sh
@@ -0,0 +1,8 @@
+pip install scikit-image
+pip install opencv-python
+pip install imgaug
+pip install lxml
+pip install pandas
+pip install selectivesearch
+pip install tensorflow-gpu==1.15.2
+pip install keras==2.3.0
---
+++ data/util/install_tf_obj.sh
@@ -0,0 +1,11 @@
+pip install scikit-image
+pip install opencv-python
+pip install imgaug
+pip install lxml
+pip install pandas
+pip install selectivesearch
+pip install tensorflow-gpu==1.15.2
+pip install keras==2.3.0
+pip install contextlib2
+conda install -c conda-forge pycocotools -y
+conda install nb_conda -y
